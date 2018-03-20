1cc3629b1b6b5a67ec4a82521bdbe118107b1e6d
J4K0B <jakob.tepe@gmail.com>
2018-03-20 18:24:48 +0100

    feat: Fully Responsive Design Added different designs for different screen sizes.

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ window.initMap = () => {
   self.map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: loc,
-    scrollwheel: false
+    scrollwheel: false,
   });
   updateRestaurants();
 }
